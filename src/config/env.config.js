4cbb038211ef220f9bf788fe8600f05f7569e605
CyberMehul <mehulrathod9254@gmail.com>
2026-05-09 18:31:23 +0530

    fix: cors bug fixed

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,10 +1,6 @@
-const isProd = process.env.NODE_ENV === "production";
-
 const config = {
-  FRONTEND_URL: isProd
-    ? process.env.FRONTEND_URL
-    : process.env.LOCAL_FRONTEND_URL,
-  BACKEND_URL: isProd ? process.env.BACKEND_URL : process.env.LOCAL_BACKEND_URL,
+  FRONTEND_URL: process.env.FRONTEND_URL || process.env.LOCAL_FRONTEND_URL,
+  BACKEND_URL: process.env.BACKEND_URL || process.env.LOCAL_BACKEND_URL,
 };
 
 export default config;
